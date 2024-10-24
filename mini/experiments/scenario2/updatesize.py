334aadb88746996e8e16024cfbd0fcc5e33b4c7b
duffcjones <86898157+duffcjones@users.noreply.github.com>
2024-10-24 02:12:26 -0400

    Using a smaller queue size for testing

--- mini/experiments/scenario2/updatesize.py
+++ mini/experiments/scenario2/updatesize.py
@@ -11,27 +11,27 @@ experiment_file = "scenario2-updatesize-{}"
 
 packet_segment_size = 8800
 waitTime = 1
-num_nodes = 3
+num_nodes = 5
 bandwidth = 1000
 latency = 2
 tree_size = 10000
 geocode = "dpwhwt"
-experimentWaitTime = 60
+experimentWaitTime = 40
 
 if __name__ == "__main__":
     Setup.packet_segment_size = packet_segment_size
     Setup.wait_time = waitTime
 
     # queue_sizes = [10,25,50,100,1000]
-    queue_sizes = [500]
+    queue_sizes = [50]
 
     for queue_size in queue_sizes:
         clear_results("/tmp/minindn")
         topo = make_topo(num_nodes, latency, bandwidth)
         results_dir = results_dir_base.format(queue_size)
         actions = [
-            [f"INIT {geocode} {tree_size} {queue_size} 5", "WAIT 40"],
-            [f"INIT {geocode} {tree_size} {queue_size} 5", "ADD /some/test/data/dpwhwtmpz0 0","PREP_QUEUE 0", "UPDATE 0", "WAIT 40"],
-            [f"INIT {geocode} {tree_size} {queue_size} 5", "WAIT 40"]
+            ["SETUP 2", f"INIT {geocode} {tree_size} {queue_size} 5", "WAIT 20"],
+            ["SETUP 2", f"INIT {geocode} {tree_size} {queue_size} 5", "ADD /some/test/data/dpwhwtmpz0 0","PREP_QUEUE 0", "UPDATE 0", "WAIT 20"],
+            ["SETUP 2", f"INIT {geocode} {tree_size} {queue_size} 5", "WAIT 20"]
         ]
         run_experiments(topo, iterations, results_dir, experiment_file.format(queue_size), actions, experimentWaitTime)
