79ec2b4bbe7cda14c30f4f74c11bfb0ae66e9494
Andrew Ning <aning97@gmail.com>
2024-11-05 20:29:37 -0500

    Fix scenario 3

--- mini/experiments/scenario2/latency.py
+++ mini/experiments/scenario2/latency.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     Setup.packet_segment_size = packet_segment_size
     Setup.batch_size = batch_size
     Setup.wait_time = waitTime
+    Setup.request_asset = request_asset
 
     actions = [
         ["SETUP 2", f"INIT {geocode} {tree_size} {queue_size} 5", "WAIT 5"],
--- mini/experiments/scenario2/treesize.py
+++ mini/experiments/scenario2/treesize.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     Setup.packet_segment_size = packet_segment_size
     Setup.batch_size = batch_size
     Setup.wait_time = waitTime
+    Setup.request_asset = request_asset
 
     tree_sizes = [5000]
     #tree_sizes = [25000]
